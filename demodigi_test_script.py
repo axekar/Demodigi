618a51cd2074a0933f652cff2cf54ae1361d541f
Alvin-Gavel <alvin.gavel@gmail.com>
2022-03-17 07:13:28 +0100

    testing how to commit to github

---
+++ demodigi_test_script.py
@@ -0,0 +1,36 @@
+import demodigi as dd
+
+print_results = True
+
+known_background_1 = dd.background("hates computers", dd.standard_transformations["subtractive normal deterioration"], dd.standard_transformations["subtractive normal deterioration"], 0.2)
+known_background_2 = dd.background("kazoo band outside office", dd.standard_transformations["no effect"], dd.standard_transformations["subtractive normal deterioration"], 0.1)
+known_backgrounds = [known_background_1, known_background_2]
+
+unknown_background_1 = dd.background("secretly a ghost", dd.standard_transformations["subtractive normal deterioration"], dd.standard_transformations["subtractive normal deterioration"], 0.1)
+unknown_backgrounds = [unknown_background_1]
+
+testgroup = dd.participants(8000, dd.standard_distributions["normal"], known_backgrounds = known_backgrounds, unknown_backgrounds = unknown_backgrounds)
+
+if print_results:
+   testgroup.describe()
+
+
+default = dd.standard_transformations["additive normal improvement (big)"]
+
+manipulation_1 = dd.manipulation("funny hats", dd.standard_transformations["additive normal improvement"])
+manipulation_2 = dd.manipulation("prayer and incense", dd.standard_transformations["additive normal improvement"])
+manipulation_3 = dd.manipulation("all text in comic sans", dd.standard_transformations["no effect"])
+manipulations = [manipulation_1, manipulation_2, manipulation_3]
+
+bounds = dd.skill_boundaries(25, 50, minimum_quality_difference = 0.1)
+
+trial_study = dd.study('test', testgroup, default, manipulations, bounds = bounds)
+if print_results:
+   trial_study.describe()
+
+trial_study.run_study()
+if print_results:
+   trial_study.summarise_results()
+   trial_study.plot_folder = 'DD_plots'
+   trial_study.plot_results()
+   trial_study.plot_participants()
