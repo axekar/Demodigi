d057a099c8f7901d0a3f8bdbd334f03341a8eecc
Alvin-Gavel <alvin.gavel@gmail.com>
2022-03-25 11:33:46 +0100

    Documented varying_n

--- behavioural_experiment.py
+++ behavioural_experiment.py
@@ -178,8 +178,6 @@ class experiment_run:
    """
    def __init__(self, n_pre, n_post, P_pre, P_post, name = ''):
       """
-      This represents a single run of the experiment
-   
       Parameters
       ----------
       n_pre : int
@@ -264,7 +262,69 @@ class experiment_run:
       return
 
 class varying_n:
+   """
+   This is intended to estimate for which numbers of participants, and for
+   which P_pre and P_post, the study can be expected to get any useful
+   results. It does this by running a large number of simulated experiment
+   runs, and for each value of n taking the median of their estimated
+
+   Optionally, it is possible to also include multiple values of P_pre and
+   P_post
+   
+   Attributes
+   ----------
+   name : str
+   \tThe name of the object. Used when saving data
+   plot_folder : str
+   \tPath to a folder where plots should be run. If none is specified,
+   \tplots are shown immediately instead.
+   n_min : int
+   \tThe smallest number of participants n to look at
+   n_max : int
+   \tThe largest number of participants n to look at
+   n_steps : int
+   \tThe number of different values of n to simulate
+   P_pre : float or list of float
+   \tThe probability that a participant who has not taken the learning
+   \tmodule will exhibit the desired behaviour
+   P_post : float or list of float
+   \tThe probability that a participant who has taken the learning
+   \tmodule will exhibit the desired behaviour
+   P_type : str
+   \tWhether one or more values of each of P_pre and P_post are used
+   P_steps : int
+   \tHow many values of each of P_pre and P_post are used
+   iterations : int
+   \tThe number of experiment_runs to generate for each combination of n,
+   \tP_pre and P_post
+   ns : float ndarray
+   \tArray of the different values of n to test
+   median_pDpos : float ndarray
+   \tArray of the medians of pDpos for the different combinations of n,
+   \tP_pre and P_post
+   """
    def __init__(self, n_min, n_max, P_pre, P_post, n_steps = 100, iterations = 100, name = ''):
+      """
+      Parameters
+      ----------
+      n_min : int
+      \tDescribed under attributes
+      n_max : int
+      \tDescribed under attributes
+      P_pre : float
+      \tDescribed under attributes
+      P_post : float
+      \tDescribed under attributes
+      
+      Optional parameters
+      -------------------
+      name : str
+      \tDescribed under attributes
+      n_steps : int
+      \tDescribed under attributes
+      iterations : int
+      \tDescribed under attributes
+      """
       self.n_min = n_min
       self.n_max = n_max
       self.n_steps = n_steps
@@ -303,6 +363,9 @@ class varying_n:
       return
    
    def run(self):
+      """
+      Start simulating experiment runs
+      """
       for i in range(self.n_steps):
          n = self.ns[i]
          for j in range(self.P_steps):
@@ -314,6 +377,9 @@ class varying_n:
       return
       
    def plot_pDpos(self):
+      """
+      Plot median pDpos as a function of n
+      """
       plt.clf()
       for m in range(self.P_steps):
          plt.plot(self.ns, self.median_pDpos[:,m], label = r'$P_{{\mathrm{{pre}}}} = {}, P_{{\mathrm{{post}}}} = {}$'.format(self.P_pre[m], self.P_post[m]))
