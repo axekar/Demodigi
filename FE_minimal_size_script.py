66d2ddb7245445b9a147c1ec23758325fbecf36a
Alvin-Gavel <alvin.gavel@gmail.com>
2022-03-30 15:27:06 +0200

    Added plotting toggle

--- FE_minimal_size_script.py
+++ FE_minimal_size_script.py
@@ -9,14 +9,18 @@ import matplotlib.pyplot as plt
 
 import factorial_experiment as fe
 
+# Makes plots describing the simulated study
+plot_results = True
+
 
 # First we simulate the minimum number of participants needed for us to
 # tell that the learning modules do anything at all
 
 absolute_minimum = fe.minimal_size_experiment('Absolute minimum', 0.5, fe.standard_transformations["minimal improvement"], 40, [], [], 10, 200, n_steps = 190, iterations = 100)
 absolute_minimum.run()
-absolute_minimum.plot_folder = 'DD_plots'
-absolute_minimum.plot()
+if plot_results:
+   absolute_minimum.plot_folder = 'DD_plots'
+   absolute_minimum.plot()
 
 
 age = fe.simulated_background("old enough to have grown up without PC", fe.standard_transformations["no effect"], fe.standard_transformations["no effect"], 0.25)
@@ -28,5 +32,6 @@ goals = fe.simulated_manipulation("Goal setting", fe.standard_transformations["m
 
 minimum = fe.minimal_size_experiment('Minimum', 0.5, fe.standard_transformations["minimal improvement"], 40, [QBL_PQBL, goals], [age, gender, motivated], 10, 200, n_steps = 190, iterations = 100)
 minimum.run()
-minimum.plot_folder = 'DD_plots'
-minimum.plot()
+if plot_results:
+   minimum.plot_folder = 'DD_plots'
+   minimum.plot()
