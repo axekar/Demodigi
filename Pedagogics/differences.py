3f4482ead10eb084e32b22271ca829401e270f00
Alvin-Gavel <alvin.gavel@gmail.com>
2022-05-31 10:06:23 +0200

    Removed code duplication

--- Pedagogics/differences.py
+++ Pedagogics/differences.py
@@ -31,53 +31,67 @@ def compare_catapults(mu_A, mu_B, sigma_A, sigma_B, n_throws):
    that the range of the individual throws follows a normal distribution.
    """
 
-   # Generate throws for each catapult   
-   throws_A = sigma_A * rd.randn(n_throws) + mu_A
-   throws_B = sigma_B * rd.randn(n_throws) + mu_B
+   if sigma_A < 0 or sigma_B < 0:
+      print('Standard deviation cannot be negative!')
+      print('Values input were {} and {}'.format(sigma_A, sigma_B))
+      return
 
-   # A negative value for mu is not strictly *wrong*, but in practise the
-   # range of a catapult is positive.
-   mu_A = abs(mu_A)
-   mu_B = abs(mu_B)
-   
+   catapults = ['A', 'B']
+   mu = {'A':abs(mu_A), 'B':abs(mu_B)}
+   sigma = {'A':sigma_A, 'B':sigma_B}
+
+   # Generate throws for each catapult   
+   throws = {}
+   for catapult in catapults:
+      throws[catapult] = sigma[catapult] * rd.randn(n_throws) + mu[catapult]
+
    # Make a grid of the possible values of mu and sigma. This would be
    # computationally infeasible if we had many parameters, but with just
    # two we can get away with it.
-   min_mu = 0
-   max_mu = 2 * max(mu_A, mu_B)
-   min_sigma = 0
-   max_sigma = 2 * max(sigma_A, sigma_B)
-   
    n_steps = 100
+   
+   min_mu = 0
+   min_sigma = 0   
+
+   max_mu = 0
+   max_sigma = 0
+   for catapult in catapults:
+      max_mu = max(max_mu, mu[catapult])
+      max_sigma = max(max_sigma, sigma[catapult])
+   max_mu = 2 * max_mu
+   max_sigma = 2 * max_sigma
 
    mu_vector = np.linspace(min_mu, max_mu, num = n_steps)
    sigma_vector = np.linspace(min_sigma, max_sigma, num = n_steps)
-
    mu_grid, sigma_grid = np.meshgrid(mu_vector, sigma_vector)
 
    # The log-prior P(mu, sigma). To stay consistent with a frequentist analysis,
    # we use a flat prior.
-   log_prior_A = np.zeros((n_steps, n_steps))
-   log_prior_B = np.zeros((n_steps, n_steps))
+   log_prior = {}
+   for catapult in catapults:
+      log_prior[catapult] = np.zeros((n_steps, n_steps))
 
    # The unnormalised log-likelihood P(throws|mu, sigma)
-   log_L_A_by_throw = np.zeros((n_steps, n_steps, n_throws))
-   log_L_B_by_throw = np.zeros((n_steps, n_steps, n_throws))
-   for i in range(n_throws):
-      log_L_A_by_throw[:,:,i] = np.log(1. / (sigma_grid * np.sqrt(2 * np.pi))) + ( - (1./2.) * ((throws_A[i] - mu_grid) / (sigma_grid))**2)
-      log_L_B_by_throw[:,:,i] = np.log(1. / (sigma_grid * np.sqrt(2 * np.pi))) + ( - (1./2.) * ((throws_B[i] - mu_grid) / (sigma_grid))**2)
-   log_L_A = np.sum(log_L_A_by_throw, axis = 2)
-   log_L_B = np.sum(log_L_B_by_throw, axis = 2)
+   log_L_by_throw = {}
+   log_L = {}
+   for catapult in catapults:
+      log_L_by_throw[catapult] = np.zeros((n_steps, n_steps, n_throws))
+      for i in range(n_throws):
+         log_L_by_throw[catapult][:,:,i] = np.log(1. / (sigma_grid * np.sqrt(2 * np.pi))) + ( - (1./2.) * ((throws[catapult][i] - mu_grid) / (sigma_grid))**2)
+      log_L[catapult] = np.sum(log_L_by_throw[catapult], axis = 2)
    
-   # The unnormalised log-posterior
-   log_P_A = log_prior_A + log_L_A
-   log_P_B = log_prior_B + log_L_B
+   # The posterior
+   log_P = {}
+   P = {}
+   for catapult in catapults:
+      log_P[catapult] = log_prior[catapult] + log_L[catapult]
+      P[catapult] = np.exp(log_P[catapult])
    
-   fig, axs = plt.subplots(2)
-   axs[0].pcolormesh(mu_grid, sigma_grid, np.exp(log_P_A))
-   axs[1].pcolormesh(mu_grid, sigma_grid, np.exp(log_P_B))
-   for ax in axs.flat:
-      ax.set(xlabel=r'$\mu$', ylabel=r'$\sigma$')
+   fig, axs = plt.subplots(len(catapults))
+   for i in range(len(catapults)):
+      catapult = catapults[i]
+      axs[i].pcolormesh(mu_grid, sigma_grid, P[catapult])
+      axs.flat[i].set(xlabel=r'$\mu$', ylabel=r'$\sigma$', title = 'Catapult {}'.format(catapult))
 
    plt.show()
    return
