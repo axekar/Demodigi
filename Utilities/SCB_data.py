0753dc6b065787488dcf9c3cace26a94430e68c1
Alvin-Gavel <alvin.gavel@gmail.com>
2022-06-03 11:26:08 +0200

    Started work on SCB_data module

---
+++ Utilities/SCB_data.py
@@ -0,0 +1,23 @@
+"""
+This module was written for the ESF-financed project Demokratisk
+Digitalisering. The project is a collaboration between
+Arbetsförmedlingen (AF) and Kungliga Tekniska Högskolan (KTH). The
+purpose of the project is to increase the digital competence - as
+defined at https://ec.europa.eu/jrc/en/digcomp - of people working at
+Arbetsförmedlingen, and by extension also job seekers. This will be
+done by learning modules at KTH using OLI-Torus.
+
+--- About this Python module ---
+
+We will need to report the progress to Statistiska Centralbyrån (SCB).
+This will involve sending them a csv file with four columns stating for
+each participant:
+
+ 1. The five-character code used to identify people at AF
+ 2. Starting date for the first module
+ 3. Estimated time spent on the first module (assumed to be 30min)
+ 4. Whether the participant has finished the first module
+
+Written by Alvin Gavel,
+https://github.com/Alvin-Gavel/Demodigi
+"""
