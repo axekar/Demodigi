0c9ac5d1a18a28d6ffd08ee694ffc043a6504e93
Alvin-Gavel <alvin.gavel@gmail.com>
2022-06-08 11:24:26 +0200

    Saved files are put in a separate directory
    It ended up being too messy having all the files generated by the script land in the same directory as the script itself, so I changed that.

--- Utilities/AIG_demonstration_script.py
+++ Utilities/AIG_demonstration_script.py
@@ -5,7 +5,8 @@ account_info_generator.
 The script simulates a group of ten participants and generates account
 names and passwords for them. It requires there to be a directory named
 Word_lists in the same directory as it is run, and this has to contain
-a file named svenska which contains a list of words.
+a file named svenska which contains a list of words. It also requires a
+directory named AIG_demo_files to put its data in.
 """
 
 import account_info_generator as aig
@@ -16,6 +17,9 @@ save_results = True
 # Test that loading the participant data works
 load_results = True
 
+# Name of the directory where saved files will be put
+save_directory = 'AIG_demo_files'
+
 wordlist = aig.read_wordlist('Word_lists/Svenska')
 
 account_info = aig.participant_list(wordlist, wordlist, password_length = 5)
@@ -25,13 +29,13 @@ account_info.simulate_participants(10)
 account_info.generate_account_data()
 
 if save_results:
-   account_info.save_data('simulated_participants_account_data.csv')
+   account_info.save_data('{}/simulated_participants_account_data.csv'.format(save_directory))
    if load_results:
-      account_info.save_simulated_participants('simulated_participants_person_data.csv')
+      account_info.save_simulated_participants('{}/simulated_participants_person_data.csv'.format(save_directory))
    
 if load_results:
    account_info_about_loaded_participants = aig.participant_list(wordlist, wordlist, password_length = 5)
-   account_info_about_loaded_participants.read_participant_data('simulated_participants_person_data.csv')
+   account_info_about_loaded_participants.read_participant_data('{}/simulated_participants_person_data.csv'.format(save_directory))
    account_info_about_loaded_participants.generate_account_data()
    if save_results:
-      account_info_about_loaded_participants.save_data('loaded_participants_account_data.csv')
+      account_info_about_loaded_participants.save_data('{}/loaded_participants_account_data.csv'.format(save_directory))
