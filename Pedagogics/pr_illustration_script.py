f842a7cd94537bcdbed3517c30484caf0c3a3566
Alvin-Gavel <alvin.gavel@gmail.com>
2022-07-05 08:25:32 +0200

    Started work on module implementing toy model about 'flat' priors
    The module priors is intended to demonstrate that: 1. There is no uniquely defined 'flat prior'. What counts as flat depends on the parametrisation of the underlying problem. 2. Frequentist methods are no solution. Exactly the same problem appears, but it is harder to diagnose.

---
+++ Pedagogics/pr_illustration_script.py
@@ -0,0 +1,11 @@
+"""
+This is a script that uses the module priors to generate the plots
+in the internal Demokratisk Digitalisering document "Ojämna priors,
+platta priors eller inga priors".
+"""
+
+import numpy as np
+import priors as pr
+
+toy_model = pr.experiment(np.pi / 4., 10, 0.1)
+toy_model.plot_data()
