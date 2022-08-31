412de869fe3fce32e0d41e68b974706b962e60ac
Alvin-Gavel <alvin.gavel@gmail.com>
2022-08-31 07:55:52 +0200

    Added comment about temporary solution

--- Utilities/PP_mapping_module_script.py
+++ Utilities/PP_mapping_module_script.py
@@ -16,7 +16,7 @@ import preprocessing as pp
 
 skills = ['WifiProblems', 'TextFormating', 'Templates', 'SpotDeepFake', 'SolvingCrash', 'SoftwareFreeze', 'SharingPictures', 'SharingLargeFiles', 'SharingFiles', 'SharingEvents', 'SearchingForInfo', 'SafePasswords', 'Phishing', 'OrganisingFiles', 'OnlineMeetingProblems', 'MapServices', 'Malware', 'LearningAboutFunctions', 'InfoGraphics', 'ImageEditingSoftware', 'GDPR', 'FreeImages', 'FindingSolutions', 'EvaluateInformation', 'EmailFunctions', 'CreatingPresentations', 'CollaborationInDocuments', 'ChangingPDFs', 'Backup']
 mod = pp.learning_module(skills)
-mod.import_oli_results('OLI_analytics/2022_09_31/raw_analytics.tsv')
+mod.import_oli_results('OLI_analytics/2022_09_31/raw_analytics.tsv') # This is obviously temporary. Later on, we will have directories for each module.
 mod.infer_participants_from_full_results()
 mod.read_participants_results()
 mod.describe_participants()
