55d23f561ef36e97e73963682b32e4875afce27c
Alvin-Gavel <alvin.gavel@gmail.com>
2022-08-31 09:35:44 +0200

    Started work on script to read results from learning module IT-säkerhet

---
+++ Utilities/PP_digital_safety_module_script.py
@@ -0,0 +1,33 @@
+"""
+This script is intended to take the results from the learning module
+IT-säkerhet and transform them into a format that can be handled by the
+module factorial_experiment, which is stored one directory further up.
+
+This course module is unusual in that it only has one session. Hence
+there is no point in plotting have participants improve over
+successive sessions.
+
+This will create a directory named 'Results', with another directory
+named 'Kartläggning' inside.
+"""
+
+import os
+import preprocessing as pp
+
+skills = ['WHF_Safety', 'Virus', 'TwoFactorAuthentication', 'Spam', 'SocialMedia', 'SafeEnvironments', 'Ransomware', 'PublicComputers', 'PortableDeviceSafety', 'PhoneFraud', 'Phishing', 'Password', 'PasswordManager', 'PUK', 'OpenNetworks', 'MacroVirus', 'InfoOverPhone', 'InfoOverInternet', 'Incognito', 'IMEI', 'GDRP_SensitivePersonalData', 'GDPR_Rights', 'GDPR_PersonalInformation', 'Cookies', 'Cache', 'Botnet', 'Backup']
+mod = pp.learning_module(skills)
+mod.import_oli_results('OLI_analytics/IT-säkerhet/2022_09_31/raw_analytics.tsv') # This is temporary. It should not actually target a specific date.
+mod.infer_participants_from_full_results()
+mod.read_participants_results()
+mod.describe_participants()
+
+try:
+   os.mkdir('Results')
+except FileExistsError:
+   pass
+try:
+   os.mkdir('Results/IT-säkerhet')
+except FileExistsError:
+   pass
+mod.export_results('Results/IT-säkerhet')
+mod.export_IDs('Results/IT-säkerhet/IDs.json')
--- Utilities/PP_mapping_module_script.py
+++ Utilities/PP_mapping_module_script.py
@@ -1,7 +1,7 @@
 """
-This script is intended to take the results from the kartläggningsmodul
-and transform them into a format that can be handled by the module
-factorial_experiment, which is stored one directory further up.
+This script is intended to take the results from the learning module
+Kartläggning and transform them into a format that can be handled by the
+module factorial_experiment, which is stored one directory further up.
 
 This course module is unusual in that it only has one session. Hence
 there is no point in plotting have participants improve over
@@ -16,7 +16,7 @@ import preprocessing as pp
 
 skills = ['WifiProblems', 'TextFormating', 'Templates', 'SpotDeepFake', 'SolvingCrash', 'SoftwareFreeze', 'SharingPictures', 'SharingLargeFiles', 'SharingFiles', 'SharingEvents', 'SearchingForInfo', 'SafePasswords', 'Phishing', 'OrganisingFiles', 'OnlineMeetingProblems', 'MapServices', 'Malware', 'LearningAboutFunctions', 'InfoGraphics', 'ImageEditingSoftware', 'GDPR', 'FreeImages', 'FindingSolutions', 'EvaluateInformation', 'EmailFunctions', 'CreatingPresentations', 'CollaborationInDocuments', 'ChangingPDFs', 'Backup']
 mod = pp.learning_module(skills)
-mod.import_oli_results('OLI_analytics/2022_09_31/raw_analytics.tsv') # This is obviously temporary. Later on, we will have directories for each module.
+mod.import_oli_results('OLI_analytics/Kartläggning/2022_09_31/raw_analytics.tsv') # This is temporary. It should not actually target a specific date.
 mod.infer_participants_from_full_results()
 mod.read_participants_results()
 mod.describe_participants()
