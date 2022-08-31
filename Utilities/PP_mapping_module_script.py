e887c3ace92d57d4a403873b6b0ce238c732d3bc
Alvin-Gavel <alvin.gavel@gmail.com>
2022-08-31 07:53:15 +0200

    Fixed creation of directory of results
    The script now attempts to create a directory named Results in the same directary as it is run in, and puts a second directory with the module name inside that. The participants' results end up being stored there.

--- Utilities/PP_mapping_module_script.py
+++ Utilities/PP_mapping_module_script.py
@@ -6,8 +6,12 @@ factorial_experiment, which is stored one directory further up.
 This course module is unusual in that it only has one session. Hence
 there is no point in plotting have participants improve over
 successive sessions.
+
+This will create a directory named 'Results', with another directory
+named 'Kartläggning' inside.
 """
 
+import os
 import preprocessing as pp
 
 skills = ['WifiProblems', 'TextFormating', 'Templates', 'SpotDeepFake', 'SolvingCrash', 'SoftwareFreeze', 'SharingPictures', 'SharingLargeFiles', 'SharingFiles', 'SharingEvents', 'SearchingForInfo', 'SafePasswords', 'Phishing', 'OrganisingFiles', 'OnlineMeetingProblems', 'MapServices', 'Malware', 'LearningAboutFunctions', 'InfoGraphics', 'ImageEditingSoftware', 'GDPR', 'FreeImages', 'FindingSolutions', 'EvaluateInformation', 'EmailFunctions', 'CreatingPresentations', 'CollaborationInDocuments', 'ChangingPDFs', 'Backup']
@@ -17,4 +21,12 @@ mod.infer_participants_from_full_results()
 mod.read_participants_results()
 mod.describe_participants()
 
-mod.export_results('Results')
+try:
+   os.mkdir('Results')
+except FileExistsError:
+   pass
+try:
+   os.mkdir('Results/Kartläggning')
+except FileExistsError:
+   pass
+mod.export_results('Results/Kartläggning')
