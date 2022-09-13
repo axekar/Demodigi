1e1a097bd0599db1386cdf35ae82537d6d7bdc4d
Alvin-Gavel <alvin.gavel@gmail.com>
2022-09-13 08:34:24 +0200

    Implemented function for file upload
    We now have a function that uploads files to Canvas. The next step will be to ensure that they actually end up where we want them. After all, the goal is to send files to each individual user, giving feedback on their performance. These files would in turn have to be created by the preprocessing module.

--- Utilities/feedback.py
+++ Utilities/feedback.py
@@ -18,5 +18,38 @@ Written by Alvin Gavel,
 https://github.com/Alvin-Gavel/Demodigi
 '''
 
-import requests
+import os
 
+import requests as r
+
+
+def upload(file_path, token):
+   """
+   Upload a file to canvas
+   """
+   file_name = file_path.split('/')[-1]
+   sz = os.stat(file_path).st_size
+   payload = {
+      'name': file_name,
+      'size': sz,
+   }
+   header = {
+   'Authorization': 'Bearer {}'.format(token)
+   }
+   
+   response_1 = r.post('https://af.instructure.com/api/v1/users/self/files', data = payload, headers=header)
+   if not 'OK' in response_1.headers['Status']:
+      print('Tried to prepare for upload')
+      print('Got unexpected status in response from Canvas:')
+      print(response_1.headers['Status'])
+   response_1_content = response_1.json()
+   upload_url = response_1_content['upload_url']
+
+   f = open(file_path, 'rb')
+   response_2 = r.post(upload_url, files = {file_name: f})
+   response_2_content = response_2.json()
+   if not response_2_content['upload_status'] == 'success':
+      print('Tried to upload')
+      print('Got unexpected status in response from Canvas:')
+      print(response_2.headers['Status'])
+   return
