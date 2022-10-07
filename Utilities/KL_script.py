c9314d7a8caadf7a91cbbc9fcb4b7e83fe1a2372
Alvin-Gavel <alvin.gavel@gmail.com>
2022-10-07 14:36:32 +0200

    Added more commentary

--- Utilities/KL_script.py
+++ Utilities/KL_script.py
@@ -29,28 +29,56 @@ makefolder('Resultat/Kartläggning')
 makefolder('Resultat/Kartläggning/Individer')
 makefolder('Resultat/Kartläggning/Återkoppling')
 makefolder('Resultat/Kartläggning/Plottar')
-
-salt = input("Skriv in det salt som används vid hashningen som skapar användarnamnen:\n")
 makefolder('Användardata')
 
+# This takes the salt which gets used by the hash function to generate
+# the usernames from the 5-character codes.
+salt = input("Skriv in det salt som används vid hashningen som skapar användarnamnen:\n")
+
+# We will need to read a list of data from the HR department. Right now
+# we assume that it has a specific file name, so this will need to be
+# updated if we get a more up-to-date list.
 hr = sdm.HR_data(salt, "HR-data/Projekt demokratisk digitalisering 220921.xlsx", "Användardata")
+
+# This generates a large number of useful files based on the user data.
 hr.generate_data()
 
+# This is a dictionary describing the four competencies tested in the
+# mapping module, and the individual skills that fall under each
+# competence. Currently we do not give feedback on "Skapa och bearbeta
+# digitalt innehåll", since we do not expect to have a learning module
+# for that competence. Hence, that part is commented out, but can be
+# commented in if our needs change.
 competencies = {'Hitta och tolka digital information':['SearchingForInfo', 'MapServices', 'EvaluateInformation', 'SpotDeepFake', 'OrganisingFiles', 'SharingFiles'],
 		'Kommunikation och samarbete':['EmailFunctions', 'SharingPictures', 'SharingEvents', 'CollaborationInDocuments',  'CreatingPresentations', 'SharingLargeFiles'],
 		#'Skapa och bearbeta digitalt innehåll':['ImageEditingSoftware', 'InfoGraphics', 'TextFormating', 'Templates', 'ChangingPDFs', 'FreeImages'],
 		'IT-säkerhet':['SafePasswords', 'Phishing', 'Malware', 'Backup', 'PortableDeviceSafety', 'GDPR'],
 		'Problemlösning i digitala miljöer':['SoftwareFreeze', 'FindingSolutions', 'LearningAboutFunctions', 'WifiProblems', 'OnlineMeetingProblems', 'SolvingCrash']}
 
+# For safety's sake, we toss out all results from earlier than the
+# mapping officially started.
 start_date = datetime.datetime(2022, 9, 27, tzinfo = pytz.UTC)
+
+# We specify that the module has only one session - in contrast to the
+# other modules where a central part of the pedagogy is the use of
+# multiple sessions.
 mod = pp.learning_module(competencies, n_sessions = 1, start_date = start_date, section_slug = 'kartlggning_av_digital_kompete_5arpp')
 
+# We read the datashop file to get the participants' results. This may
+# change if I can ever get the algorithm combining Datashop and raw_
+# analytics to work.
 mod.import_datashop('OLI_analytics/Kartläggning/datashop/Datashop_af_kartlggning_av_digital_komp.xml')
 
+# We read the list of user names that was created by the call to
+# generate_data, above.
 mod.read_participant_IDs('Användardata/Användarnamn.xlsx')
 mod.read_participants_results(database = 'datashop')
+
+# We get some terse text output describing the results on the module.
 mod.describe_module()
 
+# We export a large number of files that will be useful to different
+# people.
 mod.export_IDs('Resultat/Kartläggning/IDs.json')
 mod.export_SCB_data('Resultat/Kartläggning/Prelim_SCB_data.csv')
 hr.convert_SCB_data('Resultat/Kartläggning/Prelim_SCB_data.csv', 'Resultat/Kartläggning/SCB_data.xlsx')
@@ -60,10 +88,15 @@ mod.export_full_results('Resultat/Kartläggning/Full_results.csv')
 mod.plot_results_by_time('Resultat/Kartläggning/Plottar')
 mod.plot_initial_performance('Resultat/Kartläggning/Plottar')
 
+# To upload feedback to Canvas, it is necessary to have a token for an
+# admin account.
 token = input("Fyll i en token till ett admin-konto på Canvas:\n")
 message = 'Hej!\n\nDetta är din individuella återkoppling på kartläggningsmodulen.\n\nDetta är ett automatiserat meddelande och går inte att svara på.'
 
+# We upload the feedback.
 cc.send_feedback('Användardata/Användarnamn.xlsx', 'Resultat/Kartläggning/Återkoppling', 'alvin.gavel@arbetsformedlingen.se', 'Återkoppling på kartläggningsmodul', message, token, test = False, verbose = True)
 
+# We create a list of mail adresses to the people who just received
+# feedback.
 today = datetime.today().strftime('%Y-%m-%d')
 hr.emails_from_participant_list('Resultat/Kartläggning/Återkoppling/Tracker/Daily_deliveries/{}.txt'.format(today))
